380da2b0a3537cfe27dd6baa4bdfd54076a7af1b
Karol <karol.perec@tsh.io>
2022-02-12 00:08:57 +0100

    feat: add get entry endpoint

---
+++ amplify/backend/function/getEntry/src/index.js
@@ -0,0 +1,21 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	API_KEY
+	SECRET
+	OWM_API_KEY
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
---
+++ amplify/backend/function/getEntry/src/index.ts
@@ -0,0 +1,20 @@
+import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { WykopEntry, WykopResponse } from '@wykop-types';
+import { getAxiosInstance } from '/opt/nodejs/axios';
+import { createResponse } from '/opt/nodejs/lambdaUtils';
+import { mapEntry } from '/opt/nodejs/dataUtils';
+
+export const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
+  const { API_KEY, SECRET, OWM_API_KEY } = process.env;
+  const axios = getAxiosInstance(API_KEY!, SECRET!, OWM_API_KEY!);
+
+  const { data } = await axios.get<WykopResponse<WykopEntry>>(
+    `/entries/entry/${event.pathParameters?.id}/output/clear`
+  );
+
+  if (data.error) return createResponse(data.error, 400);
+
+  const link = mapEntry(data.data);
+
+  return createResponse(link, 200);
+};
