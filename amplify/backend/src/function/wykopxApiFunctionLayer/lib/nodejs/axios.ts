dbfc31e4d065d9f64991c2b735b6e8641e1d3f86
Karol <karol.perec@gmail.com>
2022-05-13 01:05:40 +0200

    chore: update packages and scripts

--- amplify/backend/src/function/wykopxApiFunctionLayer/lib/nodejs/axios.ts
+++ amplify/backend/src/function/wykopxApiFunctionLayer/lib/nodejs/axios.ts
@@ -4,18 +4,18 @@ import { MD5 } from 'crypto-js';
 const wykopAxiosInstance = axios.create({ baseURL: 'https://a2.wykop.pl' });
 
 wykopAxiosInstance.interceptors.request.use((config) => {
-  // if (config.method === 'POST') {
-  config.url += `/appkey/${process.env.API_KEY}`;
-  const signContent =
-    process.env.SECRET! +
-    config.baseURL +
-    config.url +
-    (config.data ? Object.values(config.data).join(',') : '');
-  const apiSign = MD5(signContent).toString();
-  config.headers = { ...config.headers, apisign: apiSign };
-  // } else {
-  //   config.url += `/appkey/${process.env.OWM_API_KEY}`;
-  // }
+  if (config.method === 'POST' || !process.env.OWM_API_KEY) {
+    config.url += `/appkey/${process.env.API_KEY}`;
+    const signContent =
+      process.env.SECRET! +
+      config.baseURL +
+      config.url +
+      (config.data ? Object.values(config.data).join(',') : '');
+    const apiSign = MD5(signContent).toString();
+    config.headers = { ...config.headers, apisign: apiSign };
+  } else {
+    config.url += `/appkey/${process.env.OWM_API_KEY}`;
+  }
 
   return config;
 });
