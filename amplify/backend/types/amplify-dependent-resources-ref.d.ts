03bf68c80b18da8a3115eaf12ac7c2e02c255e11
Karol <karol.perec@gmail.com>
2022-05-13 22:44:15 +0200

    chore: pull amplify backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,26 +1,44 @@
 export type AmplifyDependentResourcesAttributes = {
-  function: {
-    wykopxApiFunctionLayer: {
-      Arn: 'string';
-    };
-    getPromotedLinks: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-    getLink: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-  };
-  api: {
-    wykopxApi: {
-      RootUrl: 'string';
-      ApiName: 'string';
-      ApiId: 'string';
-    };
-  };
-};
+    "function": {
+        "wykopxApiFunctionLayer": {
+            "Arn": "string"
+        },
+        "getPromotedLinks": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "getLink": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "getEntries": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "getEntry": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "getHits": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "wykopxApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
