b809d1a62f6630e53728b7f1c6d9f12e07694011
Karol <karol.perec@gmail.com>
2022-05-14 01:15:14 +0200

    chore: dev

--- amplify/backend/function/wykopxApiFunctionLayer/lib/nodejs/lambdaUtils.ts
+++ amplify/backend/function/wykopxApiFunctionLayer/lib/nodejs/lambdaUtils.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { APIGatewayProxyResult } from 'aws-lambda';
 
 export const createResponse = (body: unknown, statusCode: number): APIGatewayProxyResult => ({
