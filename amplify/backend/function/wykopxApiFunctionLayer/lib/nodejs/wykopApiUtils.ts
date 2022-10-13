f34add3fad85c635b698a1e028c6cf5eaa10bb56
Karol <karol.perec@gmail.com>
2022-10-14 00:16:30 +0200

    fix: add md5 to api sign again

--- amplify/backend/function/wykopxApiFunctionLayer/lib/nodejs/wykopApiUtils.ts
+++ amplify/backend/function/wykopxApiFunctionLayer/lib/nodejs/wykopApiUtils.ts
@@ -25,7 +25,7 @@ wykopAxiosInstance.interceptors.request.use((config) => {
       config.baseURL +
       config.url +
       (config.data ? Object.values(config.data).join(',') : '');
-    config.headers?.common?.set('apisign', signContent.toString());
+    config.headers?.common?.set('apisign', MD5(signContent).toString());
   } else {
     config.url += `/appkey/${process.env.OWM_API_KEY}`;
   }
