b596ce8023dd08713b5fc786b30f5ac94b98d07f
Karol <karol.perec@gmail.com>
2022-11-18 22:59:01 +0100

    feat: unique inifinite data 2

--- src/utils/dataUtils.ts
+++ src/utils/dataUtils.ts
@@ -1,2 +1,2 @@
-export const filterUniqueEntries = <T extends { id: unknown }>(data?: T[]) =>
+export const filterUniqueData = <T extends { id: unknown }>(data?: T[]) =>
   [...new Map(data?.reverse().map((d) => [d.id, d])).values()].reverse();
