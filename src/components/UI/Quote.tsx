8fe6d50b60b28d4ad5d0cd139bbd85e8291afbc5
Karol-Perec <karol.perec@gmail.com>
2023-02-22 03:17:43 +0100

    feat: blockquote styling 2

--- src/components/UI/Quote.tsx
+++ src/components/UI/Quote.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from 'react';
 export const StyledBlockquote = styled('blockquote')(({ theme }) => ({
   borderLeft: `5px solid ${theme.palette.divider}`,
   paddingLeft: theme.spacing(2),
-  margin: [theme.spacing(1), 0, theme.spacing(1), 0].join(' '),
+  margin: [theme.spacing(1), 0, 0, 0].join(' '),
   color: theme.palette.text.disabled,
 }));
 
